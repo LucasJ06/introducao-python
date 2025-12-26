9f4e297c60e1384a97f4429430f74abcb32d0873
Lucas Juan <ljuan.santos06@gmail.com>
2025-12-26 11:31:59 -0300

    Programa de vender agua

--- if.py
+++ if.py
@@ -13,3 +13,30 @@ elif idade == 17:
 else:
     print("Você não pode beber!")
 
+
+
+#programa que vende água
+texto = """ 
+Escolha a sua água para comprar:
+(1) Água mineral natural - R$1,50
+(2) Água mineral com gás - R$2,50
+"""
+opcao = input(texto)
+valor_item = 0
+
+if opcao == "1":
+    valor_item = 1.5
+elif opcao == "2":
+    valor_item = 2.5
+
+if valor_item == 0:
+    print("Entre com a opção correta!")
+
+else: 
+    qtde = int(input("Digite a quantidade: "))
+    valor_total = valor_item * qtde
+    print(f"Sua conta deu {valor_total}")
+
+
+    
+
