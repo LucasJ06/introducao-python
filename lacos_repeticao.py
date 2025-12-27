dbeb291070544e7bd406687aacfdfd7a8a9ccb57
Lucas Juan <ljuan.santos06@gmail.com>
2025-12-27 16:11:17 -0300

    lacos de repeticao

---
+++ lacos_repeticao.py
@@ -0,0 +1,14 @@
+count = 1 #contador para sair do laco de repeticao
+while count < 10:
+    print("Entrei no laço", count)
+    count = count + 1
+
+
+#tabuada
+numero = int(input("Entre com um número para fazer a tabuada: "))
+contador = 0
+
+while contador <= 10:
+    print(numero, "*" ,contador, "=" , numero * contador)
+    contador = contador + 1
+
