45d235b751b40834b0047852d2f1d7605f7ca864
Lucas Juan <ljuan.santos06@gmail.com>
2025-12-31 19:45:41 -0300

    documentando a funcao

--- funcoes.py
+++ funcoes.py
@@ -6,6 +6,21 @@ print(f(4))
 
 #criando uma função para calcular juros compostos
 def juros_compostos(aporte, taxa, anos):
+    """
+    juros compostos serve para calcular o retorno financeiro a partir de um aporte.
+    Deve-se considerar o valor, a taxa de juros atual e o tempo (em anos) para 
+    calculo do valor a ser retornado.
+
+    aporte:
+        um numero inteiro, que represente o valor em reais
+    
+    taxa:
+        um numero entre 0 e 1 que represente o valor da taxa de juros
+
+    anos:
+        um numero inteiro >= 1 que representa o tempo que o investimento 
+        tera liquidez
+    """
     return aporte * (1 + taxa) ** anos
 
 juros_compostos(taxa = 0.13, anos = 4, aporte = 1000)
