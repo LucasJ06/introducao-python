8635b6322c3f1914f78e3a9483606aba14c9010e
Lucas Juan <ljuan.santos06@gmail.com>
2026-01-01 19:59:39 -0300

    modulos

---
+++ modulo_lib.py
@@ -0,0 +1,26 @@
+#importando toda biblioteca math
+import math
+
+math.sqrt(9)
+math.pow(2, 4)
+math.e  
+math.pi
+
+
+#importando apenas pi da biblioteca math
+from math import pi
+
+math.pi
+
+#importando pi e "e" da biblioteca math
+
+from math import pi, e 
+
+math.pi
+math.e
+
+
+#importando biblioteca e renomeando
+import math as mh
+
+mh.pow(2, 4)
